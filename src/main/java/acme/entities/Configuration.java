4fb9dce373da5b256324d96ed3933c38c7bd2cba
ignaciowarleta <nachowarleta00@gmail.com>
2023-03-09 18:45:30 +0100

    Configuration

---
+++ src/main/java/acme/entities/Configuration.java
@@ -0,0 +1,24 @@
+
+package acme.entities;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Pattern;
+
+import acme.framework.data.AbstractEntity;
+
+public class Configuration extends AbstractEntity {
+
+	/**
+	 * 
+	 */
+	private static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Pattern(regexp = "[A-Z]{3}")
+	protected String			defaultCurrency;
+
+	@NotBlank
+	@Pattern(regexp = "[A-Z]{3}")
+	protected String			acceptedCurrencies;
+
+}

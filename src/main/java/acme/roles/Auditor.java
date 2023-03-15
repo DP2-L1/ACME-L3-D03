ffdc30c81338238a4c4e48c766f14757dc29f013
jesmarver <jesusperike@gmail.com>
2023-03-15 10:17:19 +0100

    Rol Auditor añadido

---
+++ src/main/java/acme/roles/Auditor.java
@@ -0,0 +1,36 @@
+
+package acme.roles;
+
+import javax.persistence.Entity;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.URL;
+
+import acme.framework.data.AbstractRole;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Auditor extends AbstractRole {
+
+	private static final long	serialVersionUID	= 1L;
+
+	@NotBlank
+	@Size(max = 76)
+	private String				firm;
+
+	@NotBlank
+	@Size(max = 26)
+	private String				profesionalId;
+
+	@NotBlank
+	@Size(max = 101)
+	private String				certifications;
+
+	@URL
+	private String				link;
+
+}

98328f837e2eac5e83a4f4526739430f79937a97
marsannar2 <soymarcosr@gmail.com>
2023-03-15 18:12:30 +0100

    added samples for Note,bulletin and peep+fixed entities

--- src/main/java/acme/entities/Note.java
+++ src/main/java/acme/entities/Note.java
@@ -1,9 +1,11 @@
 
 package acme.entities;
 
-import java.sql.Date;
+import java.util.Date;
 
 import javax.persistence.Entity;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Past;
@@ -11,34 +13,38 @@ import javax.validation.constraints.Past;
 import org.hibernate.validator.constraints.Length;
 import org.hibernate.validator.constraints.URL;
 
+import acme.framework.data.AbstractEntity;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
 @Getter
 @Setter
-public class Note {
+public class Note extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
 
 	@Past
-	private Date	instationMoment;
+	@Temporal(value = TemporalType.TIMESTAMP)
+	private Date				instationMoment;
 
 	@NotBlank
 	@Length(max = 75)
-	private String	title;
+	private String				title;
 
 	@NotBlank
 	@Length(max = 100)
-	private String	message;
+	private String				message;
 
 	//Propiedad derivada
 	@NotBlank
 	@Length(max = 75)
-	private String	author;
+	private String				author;
 
 	@Email
-	private String	email;
+	private String				email;
 
 	@URL
-	private String	link;
+	private String				link;
 
 }

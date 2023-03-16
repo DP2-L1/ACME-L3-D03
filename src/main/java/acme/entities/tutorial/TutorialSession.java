42843630e397af290d1346a73a33961bf1dafd68
alvnavriv <alvnavriv@alum.us.es>
2023-03-16 17:06:42 +0100

    Completado excepto por relación tutorial-course

--- src/main/java/acme/entities/tutorial/TutorialSession.java
+++ src/main/java/acme/entities/tutorial/TutorialSession.java
@@ -1,6 +1,8 @@
 
 package acme.entities.tutorial;
 
+import java.util.Date;
+
 import javax.persistence.Entity;
 import javax.persistence.EnumType;
 import javax.persistence.Enumerated;
@@ -8,10 +10,11 @@ import javax.persistence.ManyToOne;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.Valid;
-import javax.validation.constraints.Future;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.URL;
 
 import acme.framework.data.AbstractEntity;
 import lombok.Getter;
@@ -38,9 +41,9 @@ public class TutorialSession extends AbstractEntity {
 
 	@NotNull
 	@Temporal(TemporalType.TIMESTAMP)
-	@Future
-	private Integer				duration;
+	private Date				duration;
 
+	@URL
 	private String				link;
 
 	@NotNull

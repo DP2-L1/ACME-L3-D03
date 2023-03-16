0bd95a86889305eec9582c3b656ff98a7d020cef
ignaciowarleta <nachowarleta00@gmail.com>
2023-03-16 22:44:15 +0100

    Entidades Auditoria

---
+++ src/main/java/acme/entities/auditor/AuditorDashboard.java
@@ -0,0 +1,33 @@
+
+package acme.entities.auditor;
+
+import javax.persistence.Entity;
+
+import acme.framework.data.AbstractEntity;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Setter
+@Getter
+public class AuditorDashboard extends AbstractEntity {
+
+	private static final long	serialVersionUID	= 1L;
+
+	private int					totalAudits;
+
+	// Number of auditing records in this audits
+	private double				averageAudits;
+	private double				deviationAudits;
+
+	private int					minimumAudits;
+	private int					maximumAudits;
+
+	// Time of the period lengths in their auditing records
+	private double				averageTime;
+	private double				deviationTime;
+
+	private int					minimumTime;
+	private int					maximumTime;
+
+}

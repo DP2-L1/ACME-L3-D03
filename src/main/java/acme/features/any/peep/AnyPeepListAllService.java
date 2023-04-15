2823e96a7ccc3f2e6067da1c9eac4b46700c8099
alvnavriv <alvnavriv@alum.us.es>
2023-04-15 17:27:00 +0200

    Repositorios y servicios añadidos

---
+++ src/main/java/acme/features/any/peep/AnyPeepListAllService.java
@@ -0,0 +1,52 @@
+
+package acme.features.any.peep;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.peep.Peep;
+import acme.framework.components.accounts.Any;
+import acme.framework.components.models.Tuple;
+import acme.framework.services.AbstractService;
+
+@Service
+
+public class AnyPeepListAllService extends AbstractService<Any, Peep> {
+
+	@Autowired
+	protected AnyPeepRepository repository;
+
+
+	@Override
+	public void check() {
+		super.getResponse().setChecked(true);
+	}
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Collection<Peep> object;
+
+		object = this.repository.findAllPeeps();
+
+		super.getBuffer().setData(object);
+	}
+
+	@Override
+	public void unbind(final Peep object) {
+		assert object != null;
+
+		Tuple tuple;
+
+		tuple = super.unbind(object, "reference", "status");
+		tuple.put("title", object.getTitle());
+
+		super.getResponse().setData(tuple);
+	}
+}
---
+++ src/main/java/acme/features/authenticated/peep/AuthenticatedPeepListAllService.java
@@ -0,0 +1,51 @@
+
+package acme.features.authenticated.peep;
+
+import java.util.Collection;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import acme.entities.peep.Peep;
+import acme.framework.components.accounts.Authenticated;
+import acme.framework.components.models.Tuple;
+import acme.framework.services.AbstractService;
+
+@Service
+public class AuthenticatedPeepListAllService extends AbstractService<Authenticated, Peep> {
+
+	@Autowired
+	protected AuthenticatedPeepRepository repository;
+
+
+	@Override
+	public void check() {
+		super.getResponse().setChecked(true);
+	}
+
+	@Override
+	public void authorise() {
+		super.getResponse().setAuthorised(true);
+	}
+
+	@Override
+	public void load() {
+		Collection<Peep> object;
+
+		object = this.repository.findAllPeeps();
+
+		super.getBuffer().setData(object);
+	}
+
+	@Override
+	public void unbind(final Peep object) {
+		assert object != null;
+
+		Tuple tuple;
+
+		tuple = super.unbind(object, "reference", "status");
+		tuple.put("title", object.getTitle());
+
+		super.getResponse().setData(tuple);
+	}
+}
